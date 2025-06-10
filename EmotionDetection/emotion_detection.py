081b804a136622b11eda37220153862926473e5e
ebenl08 <ebenlim08@gmail.com>
2025-06-10 08:58:56 -0400

    add docstring& error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -4,6 +4,8 @@
 import requests
 import json
 
+
+
 def emotion_detector(text_to_analyse):
     ''' A function to detect the emotion scores associated 
         with a given text/string input. The output is a dictionary of
@@ -14,12 +16,23 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
 
+    #Empty score dictionary
+    emotion_scores = { "anger": None, "disgust": None, "fear": None, 
+            "joy": None, "sadness": None, 
+            "dominant_emotion": None
+        }
+    
     # Send request to the model to process the text
     response = requests.post(url, json=myobj, headers=header, timeout=8.0)
+
+    # Error handling
+    if response.status_code == 400:
+        return emotion_scores
 
     # Format the response text into JSON
     formatted_response = json.loads(response.text)
 
+    #Processing Output
     emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
     dominant_emotion = max(emotion_scores, key=emotion_scores.get)
     emotion_scores["dominant_emotion"] = dominant_emotion
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+''' Flask application that are capable of detecting the emotion value
+    or scores of a given text input using the model from 
+    the Watson NLP Library 
+'''
 from flask import Flask, render_template, request
 
 from EmotionDetection.emotion_detection import emotion_detector
@@ -6,12 +10,19 @@ app = Flask("Emotion Detector")
 
 @app.route('/emotionDetector')
 def detect_emotion():
+    ''' Retrieve and print emotion scores and the dominant 
+        emotion from a given string text input
+    '''
     # Retrieve user input
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Determine the emotion scores of the text input
     emotion_scores = emotion_detector(text_to_analyze)
-    
+
+    # Handling NoneType values in emotion_scores
+    if emotion_scores['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
     # Format output message in accordance to the project requirement
     message = "For the given statement, the system response is "
     for emotion, score in emotion_scores.items():
@@ -27,6 +38,8 @@ def detect_emotion():
 
 @app.route('/')
 def render_index():
+    ''' Render the index.html (Frontend)
+    '''
     return render_template("index.html")
 
 if __name__ == "__main__":
